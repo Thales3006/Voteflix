d75c8aee06fd766293ef428142356580c57bb4cb
thales3006 <thales3006@gmail.com>
2025-08-27 00:47:31 -0300

    base frontent for the begining

--- server/src/main/java/com/thales/Main.java
+++ server/src/main/java/com/thales/Main.java
@@ -1,7 +1,22 @@
 package com.thales;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/server.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setScene(scene);
+        stage.setTitle("VoteFlix");
+        stage.show();
+    }
+
     public static void main(String[] args) {
-        System.out.println("Server: Hello world!");
+        launch(args);
     }
 }
