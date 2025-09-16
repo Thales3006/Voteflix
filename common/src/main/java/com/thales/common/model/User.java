0b1df62af3785e029080ba275e18f845742e2951
Thales Janisch Santos <thales3006@gmail.com>
2025-09-16 20:47:16 -0300

    removed id from user

--- common/src/main/java/com/thales/common/model/User.java
+++ common/src/main/java/com/thales/common/model/User.java
@@ -5,13 +5,11 @@ import lombok.Data;
 @Data
 public class User {
 
-    private int ID;
     private String username;
     private String password;
 
-    public User(String username, String password, int ID){
+    public User(String username, String password){
         this.username = username;
         this.password = password;
-        this.ID = ID;
     }
 }
