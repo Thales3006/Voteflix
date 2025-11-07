bde19d8dac3158386a913322a0e38bfe28172a34
Thales Janisch Santos <thales3006@gmail.com>
2025-11-07 11:54:27 -0300

    server side review done

--- common/src/main/java/com/thales/common/model/Review.java
+++ common/src/main/java/com/thales/common/model/Review.java
@@ -2,6 +2,8 @@ package com.thales.common.model;
 
 import java.time.LocalDate;
 
+import com.google.gson.JsonObject;
+
 import lombok.Data;
 
 @Data
@@ -23,5 +25,50 @@ public class Review {
         this.title = title;
         this.description = description;
         this.date = date;
+    }
+
+    public JsonObject toJson() {
+        JsonObject json = new JsonObject();
+        if (ID != null) json.addProperty("ID", ID.toString());
+        if (movieID != null) json.addProperty("movieID", movieID.toString());
+        if (userID != null) json.addProperty("userID", userID.toString());
+        if (rating != null) json.addProperty("rating", rating.toString());
+        if (title != null) json.addProperty("title", title);
+        if (description != null) json.addProperty("description", description);
+        if (date != null) json.addProperty("date", date.toString());
+        return json;
+    }
+    public static Review fromJson(JsonObject json) {
+        Integer ID = null;
+        Integer movieID = null;
+        Integer userID = null;
+        Integer rating = null;
+        String title = null;
+        String description = null;
+        LocalDate date = null;
+
+        if (json.has("ID")) {
+            ID = Integer.valueOf(json.get("ID").getAsString());
+        }
+        if (json.has("movieID")) {
+            movieID = Integer.valueOf(json.get("movieID").getAsString());
+        }
+        if (json.has("userID")) {
+            userID = Integer.valueOf(json.get("userID").getAsString());
+        }
+        if (json.has("rating")) {
+            rating = Integer.valueOf(json.get("rating").getAsString()); 
+        }
+        if (json.has("title")) {
+            title = json.get("title").getAsString();
+        }
+        if (json.has("description")) {
+            description = json.get("description").getAsString();
+        }
+        if (json.has("date")) {
+            date = LocalDate.parse(json.get("date").getAsString());
+        }
+
+        return new Review(ID, movieID, userID, rating, title, description, date);
     }
 }
