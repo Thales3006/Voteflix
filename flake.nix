8923d6ec148438acc527d8f011f4312fe3acbfe0
Thales Janisch Santos <thales3006@gmail.com>
2026-02-16 23:57:26 -0300

    feat: run methods in flake.nix

--- flake.nix
+++ flake.nix
@@ -1,4 +1,3 @@
-
 {
   description = "JavaFX + Maven dev shell";
 
@@ -8,6 +7,7 @@
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
+
       javafxLibs = with pkgs; [
         gtk3
         glib
@@ -22,6 +22,20 @@
         xorg.libXrandr
         xorg.libXinerama
       ];
+
+      javaEnv = ''
+        export JAVA_HOME=${pkgs.jdk23}
+        export PATH=${pkgs.jdk23}/bin:${pkgs.maven}/bin:$PATH
+        export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath javafxLibs}
+      '';
+
+      mkJavaApp = module: pkgs.writeShellScript "run-${module}" ''
+        ${javaEnv}
+
+        mvn -pl common install -DskipTests
+        mvn -pl ${module} javafx:run
+      '';
+
     in {
 
       devShells.${system}.default = pkgs.mkShell {
@@ -35,5 +49,16 @@
         '';
       };
 
+      apps.${system} = {
+        server = {
+          type = "app";
+          program = toString (mkJavaApp "server");
+        };
+
+        client = {
+          type = "app";
+          program = toString (mkJavaApp "client");
+        };
+      };
     };
 }
