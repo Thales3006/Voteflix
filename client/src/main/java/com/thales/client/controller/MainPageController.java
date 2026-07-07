72c9e6478b5b7e49b16382a10d9877bc1a76c503
Thales Janisch Santos <thales3006@gmail.com>
2026-07-07 20:03:49 -0300

    refactor(client): move connect/disconnect controls to login page

--- client/src/main/java/com/thales/client/controller/MainPageController.java
+++ client/src/main/java/com/thales/client/controller/MainPageController.java
@@ -1,40 +1,13 @@
 package com.thales.client.controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 
 public class MainPageController extends SceneController {
 
     @FXML private BorderPane currentPage;
-    @FXML private TextField IPField;
-    @FXML private TextField portField;
-    @FXML private Button connectButton;
-    @FXML private Button disconnectButton;
 
     @FXML protected void initialize() {
         handle(null, () -> switchContent(currentPage, "/login_page.fxml"));
-
-        connectButton.disableProperty().bind(clientService.getConnected());
-        disconnectButton.disableProperty().bind(clientService.getConnected().not());
-    }
-
-    // ===================================
-    //  UI interaction handlers
-    // ===================================
-
-    @FXML private void HandleConnectButton(ActionEvent event){
-        handle(event, () -> {
-            clientService.connect(IPField.getText(), Integer.parseInt(portField.getText()));
-        });
-        
-    }
-
-    @FXML private void HandleDisconnectButton(ActionEvent event){
-        handle(event, () -> {
-            clientService.close();
-        });
     }
 }
